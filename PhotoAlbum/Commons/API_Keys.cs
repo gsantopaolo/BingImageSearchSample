2ac89b6946da58a84ebf3fbeea250d012d800870
Gian Paolo Santopaolo <g_santopaolo@hotmail.com>
2016-01-15 02:21:10 +0100

    api keys

---
+++ PhotoAlbum/Commons/API_Keys.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoAlbum.Commons
+{
+    internal static class API_Keys
+    {
+        public static class Bing_Image
+        {
+            private const string USER_ID = "£"; // yourLiveId
+            private const string SECURE_ACCOUNT_ID = "";  
+        }
+    
+    }
+}
